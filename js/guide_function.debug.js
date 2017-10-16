25a2af93a5f9aac44b39ce97308a790147f59a91
ministori <ministori@gmail.com>
2017-10-16 13:39:41 +0900

    v2.0.11 2017/10/16 퍼블리싱 리스트 리팩토링

--- js/guide_function.debug.js
+++ js/guide_function.debug.js
@@ -1,12 +1,19 @@
 $(function(){
 
+  /**
+   * define
+   */
+
   var item = [];
   var itemFlag = [];
   var fileData = [];
-  var wholeCount = 0;
-  var doneCount = 0;
 
-  $.getJSON('file_list.json', function(data){
+  var $record = $('.file-list tbody tr');
+
+  var count = new Array(11).fill(0);
+
+  // each data split filename and ext
+  function splitData(data){
 
     $.each(data, function(key, val){
 
@@ -14,54 +21,158 @@ $(function(){
 
     });
 
-  }).done(function(){
+  }
 
-    $('.file-list tbody tr').each(function(){
+  // add class name each case ex. cancel/category/link/page(O/X)
+  function addClassname($record){
+
+    var $children = $record.children('td');
+    var childrenText = $children.eq(6).text().toLowerCase();
+
+    if($children.has('s').length){
+
+      $record.addClass('cancel');
+
+    } else {
+
+      childrenText = childrenText ? childrenText + '-' : childrenText;
+
+      $record.addClass(childrenText + 'category');
+
+    }
+
+  }
+
+  // find same filename and put link html code or X
+  function compareFile($record){
+
+    if(!$record.hasClass('cancel') && !$record.hasClass('category')){
 
       for(var j=0; j<item.length; j++){
 
-        if( $(this).children('td:last-child').text() == item[j] ){
+        if( $record.children('td:last-child').text() == item[j] ){
 
-          $(this).append('<td class="center"><a href="../html/' + item[j] + '.html" class="list-link" target="blonk"> DONE </a></td>');
+          $record.addClass('done').append('<td class="center"><a href="../html/' + item[j] + '.html" class="list-link" target="blank"> DONE </a></td>');
           itemFlag[j] = true;
           break;
 
         } else {
 
           if( j == item.length-1 ){
-            $(this).append('<td class="center">X</td>');
+            $record.append('<td class="center">X</td>');
           }
 
         }
       }
 
-      if( $(this).children('td').has('s').length ){
-        $(this).addClass('cancel');
-      } else {
-        if($(this).find('.list-link').length){
-          $(this).addClass('done');
-          doneCount++;
-        }
-        wholeCount++;
-      }
+    } else {
 
-    });
+      $record.append('<td></td>');
 
-    $('.page-whole-num').text(wholeCount);
-    $('.page-current-num').text(doneCount);
-    $('.progress-bar').css({width : Math.floor(doneCount/wholeCount*100) + '%'}).html('<div class="progress-percent">' + Math.floor(doneCount/wholeCount*100) + '%</div>');
+    }
+  }
 
+  // find unmatch filename and log
+  function unmatchFile(){
     for(var k=0; k<itemFlag.length; k++){
       if( itemFlag[k] != true ){
         fileData.push(item[k]);
       }
     }
-
     console.log(fileData);
+  }
+
+  // write each count number
+  function outputProgress(){
+
+    $record.each(function(){
+
+      var className = $(this).attr('class');
+
+      if( className == 'cancel' ){
+
+        // number of cancel page
+        count[0]++;
+
+      } else if( className == 'category' ){
+
+        // number of category
+        count[1]++;
+
+      } else {
+
+        // number of working page
+        count[2]++;
+
+        if( className.toLowerCase().indexOf('html') >= 0 ){
+          // number of html page
+          count[3]++;
+        } else if(className.toLowerCase().indexOf('develop') >= 0){
+          // number of develop page
+          count[4]++;
+        } else if(className.toLowerCase().indexOf('board') >= 0){
+          // number of board page
+          count[5]++;
+        } else if(className.toLowerCase().indexOf('link') >= 0){
+          // number of link
+          count[6]++;
+        }
+
+        if(className.toLowerCase().indexOf('done') >= 0){
+          // number of done page
+          count[7]++;
+
+          if( className.toLowerCase().indexOf('html') >= 0 ){
+            // number of done html page
+            count[8]++;
+          } else if(className.toLowerCase().indexOf('develop') >= 0){
+            // number of done develop page
+            count[9]++;
+          } else if(className.toLowerCase().indexOf('board') >= 0){
+            // number of done board page
+            count[10]++;
+          }
+        }
+
+      }
+
+    });
+
+    console.log(count);
+
+    //$('.page-whole-num').text(wholeCount);
+    //$('.page-current-num').text(doneCount);
+    //$('.progress-bar').css({width : Math.floor(doneCount/wholeCount*100) + '%'}).html('<div class="progress-percent">' + Math.floor(doneCount/wholeCount*100) + '%</div>');
+  }
+
+  /**
+   * run
+   */
+
+  // ajax
+  $.getJSON('file_list.json', function(data){
+
+    splitData(data);
+
+  }).done(function(){
+
+    $record.each(function(){
+
+      addClassname($(this));
+
+      compareFile($(this));
+
+    });
+
+    unmatchFile();
+
+    outputProgress();
 
   });
 
-  $('body').on('click', 'tr', function(e){
+
+  // link or alert message
+  $('body').on('click', '.file-list tr', function(e){
 
     if( $(this).find('.list-link').length ){
 
